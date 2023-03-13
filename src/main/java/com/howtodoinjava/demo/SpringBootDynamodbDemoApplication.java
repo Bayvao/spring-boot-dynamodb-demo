ecaad58960b2e0d441e39d3ce240fd10a3bdbc67
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2023-03-13 23:38:57 +0530

    first commit

---
+++ src/main/java/com/howtodoinjava/demo/SpringBootDynamodbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDynamodbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDynamodbDemoApplication.class, args);
+    }
+
+}
